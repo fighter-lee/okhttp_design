6194beeed107fdb6b52c566cb6a1c8eb1d1c2a35
fighter-lee <fighterlee@gmail.com>
2020-04-26 23:14:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/okhttp_design/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.okhttp_design;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    OkHttpClient client = new OkHttpClient();
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+            }
+        });
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    public void click_test1(View view) {
+        try {
+            String run = run("https://raw.github.com/square/okhttp/master/README.md");
+            Log.d(TAG, "click_test1: response:" + run);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
